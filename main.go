69feff151ff62ab3ba4d108c3e171806be27ae36
Ashwin Chatterji <ashwinath@hotmail.com>
2023-03-31 15:11:41 +0800

    Telegram bot API with main skeleton complete.

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
 
+	"github.com/ashwinath/money-tracker-telegram/config"
+	"github.com/ashwinath/money-tracker-telegram/telegram"
+)
+
+func main() {
+	configPath := flag.String("config", "", "Path to config file")
+	flag.Parse()
+
+	c, err := config.New(*configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, err := telegram.New(c.APIKey, c.Debug)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t.Run()
 }
